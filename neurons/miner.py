7b95da56d8bae374014cd98578bbbf0db7bc72b0
lefeverela <laurent.lefevere@gmail.com>
2024-02-28 12:33:04 +0000

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -90,7 +90,7 @@ def main( config ):
     This function takes the configuration and starts the miner.
     It sets up the necessary Bittensor objects, attaches the necessary functions to the axon, and starts the main loop.
     """
-    twitter_query = get_query(QueryType.TWITTER, QueryProvider.TWITTER_SCRAPERV2)
+    twitter_query = get_query(QueryType.TWITTER, QueryProvider.TWITTER_SCRAPER)
     reddit_query = get_query(QueryType.REDDIT, QueryProvider.REDDIT_SCRAPER_LITE)
     # Activating Bittensor's logging with the set configurations.
     bt.logging(config=config, logging_dir=config.full_path)
