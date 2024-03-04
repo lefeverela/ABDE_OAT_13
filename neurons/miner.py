11b0c94a0fff6b016a03e725538f70522a251a50
lefeverela <laurent.lefevere@gmail.com>
2024-03-04 00:20:20 +0000

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -69,7 +69,7 @@ def get_config():
             'miner',
         )
     )
-    config.axon.max_workers = 30
+    config.axon.max_workers = 100
     
     # Ensure the directory for logging exists, else create one.
     if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
